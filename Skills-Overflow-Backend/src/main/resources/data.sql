cfa675fbbf3c23ca3e46bac95d8202da570e3387
Gabi Petrescu <gabipetrescu98@gmail.com>
2020-05-14 19:25:40 +0300

    parola modificata la insert

--- Skills-Overflow-Backend/src/main/resources/data.sql
+++ Skills-Overflow-Backend/src/main/resources/data.sql
@@ -3,12 +3,12 @@ insert into role values(2, 'approved user');
 insert into role values(3, 'pending user');
 insert into role values(4, 'blocked user');
 
+//parola pentru userii de mai jos este "parola"- apare asa pentru ca trebuie adaugata direct criptata daca se face prin insert
 
-
-insert into user values(1, false,'george.popescu.db@gmail.com', true, 'deliric','lastname','parola','Popica', 1);
-insert into user values(2, false,'george.pop@gmail.com', true, 'gigi','lastname','parola','username', 2);
-insert into user values(3, false,'george@gmail.com', true, 'coco','lastname','parola','username', 3);
-insert into user values(4, false,'cristos@gmail.com', true, 'data','lastname','parola','username', 4);
+insert into user values(1, false,'george.popescu.db@gmail.com', true, 'deliric','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','Popica', 1);
+insert into user values(2, false,'george.pop@gmail.com', true, 'gigi','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 2);
+insert into user values(3, false,'george@gmail.com', true, 'coco','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 3);
+insert into user values(4, false,'cristos@gmail.com', true, 'data','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 4);
 
 
 
