a20ab4f05691da8b612c9bf814a91c5682f47926
CatalinCsibi <csibicatalin@gmail.com>
2020-05-17 19:03:02 +0300

    data sql changed

--- Skills-Overflow-Backend/src/main/resources/data.sql
+++ Skills-Overflow-Backend/src/main/resources/data.sql
@@ -6,19 +6,14 @@ insert into role values(5, 'declined user');
 
 //parola pentru userii de mai jos este "parola"- apare asa pentru ca trebuie adaugata direct criptata daca se face prin insert
 
-insert into user values(1, false,'george.popescu.db@gmail.com', true, 'deliric','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','Popica', 1);
-insert into user values(2, false,'george.pop@gmail.com', true, 'gigi','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 2);
-insert into user values(3, false,'george@gmail.com', true, 'coco','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 3);
-insert into user values(4, false,'cristos@gmail.com', true, 'data','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 4);
-
-<<<<<<< HEAD
-=======
-insert into user values(1, 0, false,'george.popescu@gmail.com', true, 'deliric', 'lastname','parola','Popica', 1);
-insert into user values(2, 0, false,'george.pop@gmail.com', true, 'gigi', 'lastname','parola','username', 2);
-insert into user values(3, 0, false,'george@gmail.com', true, 'coco', 'lastname','parola','user', 3);
-insert into user values(4, 0, false,'cristos@gmail.com', true, 'data','lastname','parola','user4', 4);
+insert into user values(1, 0, false,'george.popescu.db@gmail.com', true, 'deliric','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','Popica', 1);
+insert into user values(2, 0, false,'george.pop@gmail.com', true, 'gigi','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 2);
+insert into user values(3, 0, false,'george@gmail.com', true, 'coco','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 3);
+insert into user values(4, 0, false,'cristos@gmail.com', true, 'data','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 4);
 insert into user values(5, 0, false,'cristos@gmail.com', true, 'data','lastname','parola','user4', 5)
->>>>>>> bugsSolving
+
+
+
 
 
 
