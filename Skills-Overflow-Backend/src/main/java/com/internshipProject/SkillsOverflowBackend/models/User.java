ad39af9d5c8dad074f5611ee4a7b04df052089df
mihaiapostol1995 <mihaiapostol1995@gmail.com>
2020-05-18 18:07:31 +0300

    i am small

--- Skills-Overflow-Backend/src/main/java/com/internshipProject/SkillsOverflowBackend/models/User.java
+++ Skills-Overflow-Backend/src/main/java/com/internshipProject/SkillsOverflowBackend/models/User.java
@@ -64,7 +64,7 @@ public class User {
 
     //??? + json_ignore
     @ManyToMany(mappedBy = "users")
-    @JsonIgnore
+    //@JsonIgnore
     private List<Notification> notifications;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
