62f400d5c2ff97875a19b2bfadbcae34f8bed226
gheorghe <gicu.jianu@gmail.com>
2020-05-22 10:35:21 +0300

    db h2o  minor change

--- Skills-Overflow-Backend/src/main/resources/data.sql
+++ Skills-Overflow-Backend/src/main/resources/data.sql
@@ -8,7 +8,7 @@ insert into role values(5, 'declined user');
 
 insert into user values(1, 0, false,'george.popescu@gmail.com', true, 'deliric','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','Popica', 1);
 insert into user values(2, 0, false,'george.pop@gmail.com', true, 'gigi','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 2);
-insert into user values(3, 0, false,'george@gmail.com', true, 'coco','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 3);
+insert into user values(3, 0, false,'george@gmail.com', true, 'coco','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 2);
 insert into user values(4, 0, false,'cristos@gmail.com', true, 'data','lastname','$2a$04$pMF92/IeIwj7qmUEHYxFb.RrzoIh5i/jPsPXl7QV8JxVhQqUtKfry','username', 4);
 --insert into user values(5, 0, false,'cristos@gmail.com', true, 'data','lastname','parola','user4', 5);
 
